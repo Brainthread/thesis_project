ffa55a4ffc18ce08f919e8ef089fd5fbf3319f0a
Kei <54135223+Brainthread@users.noreply.github.com>
2026-02-17 14:24:30 +0100

    Add LOD sliders

---
+++ LODManager.gd
@@ -0,0 +1,4 @@
+extends Node
+class_name LODManager
+var LOD_cascades:int = 4
+var LOD_distance:float = 0.8
--- parameter_handler.gd
+++ parameter_handler.gd
@@ -3,6 +3,9 @@ class_name ParameterHandler
 
 @export var resolution_scale:int = 1
 @export var jitter_scale:int = 1
+var z_depth:int = 75
+var lod_cascades:int = 4
+var lod_distance:float = 10
 @export var color_depth:int = 24
 @export var do_affine_mapping:bool = true
 @onready var overlay_compositor = preload("res://Scripts/Shaders/ps1_compositor.tres")
@@ -11,6 +14,10 @@ class_name ParameterHandler
 @export var jitter_slider:HSlider
 @export var color_slider:HSlider
 @export var affine_toggle:CheckButton
+@export var depth_slider:HSlider
+@export var lod_cascade_slider:HSlider
+@export var lod_distance_slider:HSlider
+
 var textvalue = ""
 var settings_visible = false
 @export var control_node:Control
@@ -19,14 +26,20 @@ var settings_visible = false
 @export var generate_parameters_button:Button
 
 func _ready() -> void:
-	resolution_scale = randi_range(resolution_slider.min_value, resolution_slider.max_value)
-	jitter_scale = randf_range(jitter_slider.min_value, jitter_slider.max_value)
-	color_depth = randi_range(color_slider.min_value, color_slider.max_value)
+	resolution_scale = randi_range(round(resolution_slider.min_value), round(resolution_slider.max_value))
+	jitter_scale = randi_range(round(jitter_slider.min_value), round(jitter_slider.max_value))
+	color_depth = randi_range(round(color_slider.min_value), round(color_slider.max_value))
+	z_depth = randi_range(round(depth_slider.min_value), round(depth_slider.max_value))
+	lod_cascades = randi_range(round(lod_cascade_slider.min_value), round(lod_cascade_slider.max_value))
+	lod_distance = randf_range(lod_distance_slider.min_value, lod_distance_slider.max_value)
+	
 	do_affine_mapping = true if randi_range(0, 6) >= 4 else false
 	_on_setting_changed()
+	
 	resolution_slider.value = resolution_scale
 	jitter_slider.value = jitter_scale
 	color_slider.value = color_depth
+	depth_slider.value = z_depth
 	
 	resolution_slider.value_changed.connect(set_resolution)
 	color_slider.value_changed.connect(set_color_depth)
@@ -34,6 +47,7 @@ func _ready() -> void:
 	affine_toggle.toggled.connect(set_affine_mapping)
 	generate_parameters_button.pressed.connect(set_parameter_string)
 	copy_to_clip_button.pressed.connect(set_clip)
+	depth_slider.value_changed.connect(set_depth)
 	toggle_visible()
 
 func toggle_visible():
@@ -57,6 +71,11 @@ func set_color_depth(_value):
 	color_depth = depth
 	_on_setting_changed()
 
+func set_depth(_value):
+	var depth = round(depth_slider.value)
+	z_depth = depth
+	_on_setting_changed()
+
 func set_affine_mapping(value):
 	do_affine_mapping = value
 	_on_setting_changed()
@@ -66,6 +85,14 @@ func set_clip():
 
 func set_parameter_string():
 	output_text.text = textvalue
+
+func set_lod_cascades(value):
+	lod_cascades = round(value)
+	_on_setting_changed()
+
+func set_lod_distance(value):
+	lod_distance = value
+	_on_setting_changed()
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("one"):
@@ -81,4 +108,7 @@ func _on_setting_changed():
 	var jitter_resolution = [1280/jitter_scale, 960/jitter_scale]
 	RenderingServer.global_shader_parameter_set("resolution", jitter_resolution)
 	RenderingServer.global_shader_parameter_set("do_affine_texture_mapping", do_affine_mapping)
-	textvalue = "rs:" + str(resolution_scale) + ",\njs:" + str(jitter_scale) + ",\ncd:" + str(color_depth) + ",\nam:" + str(do_affine_mapping)
+	RenderingServer.global_shader_parameter_set("depth_quantization", z_depth)
+	LODCascades.LOD_cascades = lod_cascades
+	LODCascades.LOD_distance = lod_distance
+	textvalue = "rs:" + str(resolution_scale) + ",\njs:" + str(jitter_scale) + ",\ncd:" + str(color_depth) + ",\nam:" + str(do_affine_mapping) + ",\ndq" + str(z_depth)
